6142ad2441c5992a2e0d5c8ee0ab203016ffa37a
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-07-12 08:29:52 +0100

    ci: scaffold skeleton astro config

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,15 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  srcDir: "./app",
+  site: "https://example.com",
+  env: { validateSecrets: true, schema: {} },
+  experimental: {},
+  integrations: [],
+  prefetch: { prefetchAll: true, defaultStrategy: "viewport" },
+  session: { driver: "netlify-blobs" },
+  markdown: {},
+  image: {},
+});
