448372c106a8f50fa3d716f584ec7a0494eed8a0
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-10-30 00:22:11 +0100

    ci: update project types and settings

--- app/@types/global.d.ts
+++ app/@types/global.d.ts
@@ -1,8 +1,19 @@
-/* eslint-disable no-var */
 declare namespace globalThis {
-  interface ObjectConstructor {
-    entries<T extends NonNullable<unknown>>(object: T): readonly Entry<T>[];
-  }
   var __singletons__: Map<string, unknown> | undefined;
-  var Theme: { selectThemeIcon(theme?: string): void };
+
+  interface ThemeController {
+    value: "system" | "light" | "dark";
+    next(): void;
+    apply(): void;
+    subscribe(callback: (theme: "system" | "light" | "dark") => void): () => void;
+
+    selectThemeIcon(theme?: string): void;
+  }
+
+  // Actual instances on the global object
+  var Theme: ThemeController;
+
+  interface Window {
+    [Symbol.for("app.theme")]: ThemeController;
+  }
 }
